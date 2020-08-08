8e7c04f6892eb6f4dd497ae6469c87b7f13e3c4c
edgar.chan <edgar.chan@cloverhealth.com>
2020-08-08 11:41:51 +0800

    update robot sim solution

--- robot-simulator/lib/robot_simulator.ex
+++ robot-simulator/lib/robot_simulator.ex
@@ -6,6 +6,12 @@ defmodule RobotSimulator do
   @turn_left_map %{:north => :west, :west => :south, :south => :east, :east => :north}
   @turn_right_map %{:north => :east, :east => :south, :south => :west, :west => :north}
 
+  defguard is_valid_position(position) when is_tuple(position) and tuple_size(position) == 2
+
+  defguard is_valid_position(x, y) when is_integer(x) and is_integer(y)
+
+  defguard is_valid_direction(direction) when direction in [:north, :south, :east, :west]
+
   @doc """
   Create a Robot Simulator given an initial direction and position.
 
@@ -13,21 +19,20 @@ defmodule RobotSimulator do
   """
   def create(direction \\ :north, position \\ {0, 0})
 
-  def create(direction, _)
-      when not (is_atom(direction) and direction in [:north, :south, :east, :west]) do
+  def create(direction, _) when not is_valid_direction(direction) do
     {:error, "invalid direction"}
   end
 
-  def create(_, position) when not (is_tuple(position) and tuple_size(position) == 2) do
+  def create(_, position) when not is_valid_position(position) do
     {:error, "invalid position"}
   end
 
-  def create(_, {x, y}) when not is_integer(x) or not is_integer(y) do
+  def create(_, {x, y}) when not is_valid_position(x, y) do
     {:error, "invalid position"}
   end
 
   @spec create(direction :: atom, position :: {integer, integer}) :: any
-  def create(direction, position) do
+  def create(direction, position = {x, y}) do
     %Robot{direction: direction, position: position}
   end
 
@@ -41,15 +46,20 @@ defmodule RobotSimulator do
     {:error, msg}
   end
 
-  def simulate(robot, ?A) do
-    {x, y} = robot.position
+  def simulate(%Robot{direction: :north, position: {x, y}} = robot, ?A) do
+    %Robot{robot | position: {x, y + 1}}
+  end
 
-    case robot.direction do
-      :north -> %Robot{robot | position: {x, y + 1}}
-      :south -> %Robot{robot | position: {x, y - 1}}
-      :east -> %Robot{robot | position: {x + 1, y}}
-      :west -> %Robot{robot | position: {x - 1, y}}
-    end
+  def simulate(%Robot{direction: :south, position: {x, y}} = robot, ?A) do
+    %Robot{robot | position: {x, y - 1}}
+  end
+
+  def simulate(%Robot{direction: :east, position: {x, y}} = robot, ?A) do
+    %Robot{robot | position: {x + 1, y}}
+  end
+
+  def simulate(%Robot{direction: :west, position: {x, y}} = robot, ?A) do
+    %Robot{robot | position: {x - 1, y}}
   end
 
   def simulate(robot, ?L) do
